eb64bbbd314c8626219fd6cd9f40a08776f0ba0f
endelcomel <66982299+endelcomel@users.noreply.github.com>
2025-01-28 13:50:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,19 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export",
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tsel.mm.bing.net",
+        pathname: "/th/**",
+      },
+    ],
   },
 }
 
-mergeConfig(nextConfig, userConfig)
+module.exports = nextConfig
 
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
